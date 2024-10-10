ecdbe4c1c0bcbe8f4ca2049d6dec68ad5dccecd9
makisvan <makisvan@outlook.com>
2024-10-10 14:41:20 +0200

    Bytte plats på kod och la till variabel invalid input

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Roman_numerals
 {
@@ -10,62 +11,73 @@ namespace Roman_numerals
             while (bajskorv == "Y")
             {
                 Dictionary<char, int> RomanNumeral = new Dictionary<char, int>()
-            {
-                { 'I',1}, { 'V',5}, { 'X',10}, { 'L',50}, { 'C',100}, { 'D',500}, { 'M',1000}
-
-            };
+                {
+                    { 'I', 1 }, { 'V', 5 }, { 'X', 10 }, { 'L', 50 }, { 'C', 100 }, { 'D', 500 }, { 'M', 1000 }
+                };
 
                 Console.WriteLine("Write a roman numeral ex. I/V/X/L/C/D/M");
                 string input = Console.ReadLine().ToUpper();
-                // int repeatCount = 1;
 
-                if (input.Length > 11 )
+                // Kontrollera om inmatningen är för lång
+                if (input.Length > 11)
                 {
-                    Console.WriteLine("Input must be 11 or lower");
-                    break;
+                    Console.WriteLine("Input must be 11 characters or less.");
+                    continue;
                 }
-                
-                //if (input[i] > 0 && input[i] == input[input - 1])
-                //{
-                //    repeatCount++;
-                //}
-                //else 
-                //{
-                //    repeatCount = 1;
-                //}
-                //switch (input[i])
-                //{
-                //    case 'I':
-                //    case 'X':
-                //    case 'C':
-
-                //        if (repeatCount > 3)
-                //        {
-                //            Console.WriteLine("Invalid input, cannot be more than 3 of the same symbol in a row");
-                //            return;
-                //        }
-                //        break;
-
-                //    case 'V':
-                //    case 'L':
-                //    case 'D':
-                //        if (repeatCount > 1)
-                //        {
-                //            Console.WriteLine("Invalid input, cannot be more then 1 of the same symbol in a row");
-                //            return;
-                //        }
-                //        break;
-                //}
 
                 int total = 0;
+                int repeatCount = 1;
+                bool invalidInput = false; // Flagga för att hantera ogiltig inmatning
 
                 for (int i = 0; i < input.Length; i++)
                 {
+                    // Kontrollera om tecknet finns i uppslagslistan
                     if (!RomanNumeral.ContainsKey(input[i]))
                     {
-                        Console.WriteLine("Invalid input, try again");
+                        Console.WriteLine("Invalid input, try again.");
+                        invalidInput = true;
                         break;
                     }
+
+                    // Kontrollera om samma tecken upprepas flera gånger
+                    if (i > 0 && input[i] == input[i - 1])
+                    {
+                        repeatCount++;
+                    }
+                    else
+                    {
+                        repeatCount = 1; // Återställ räknaren om ett nytt tecken dyker upp
+                    }
+
+                    // Använd switch för att kontrollera om antalet upprepade tecken är giltigt
+                    switch (input[i])
+                    {
+                        case 'I':
+                        case 'X':
+                        case 'C':
+                            if (repeatCount > 3)
+                            {
+                                Console.WriteLine("Invalid input: Cannot be more than 3 of the same symbol in a row.");
+                                invalidInput = true;
+                            }
+                            break;
+                        case 'V':
+                        case 'L':
+                        case 'D':
+                            if (repeatCount > 1)
+                            {
+                                Console.WriteLine("Invalid input: Cannot be more than 1 of the same symbol in a row.");
+                                invalidInput = true;
+                            }
+                            break;
+                    }
+
+                    if (invalidInput)
+                    {
+                        break; // Stoppa loopen om ogiltig inmatning hittas
+                    }
+
+                    // Om nuvarande siffra är mindre än nästa, subtrahera, annars addera
                     if (i + 1 < input.Length && RomanNumeral[input[i]] < RomanNumeral[input[i + 1]])
                     {
                         total -= RomanNumeral[input[i]];
@@ -76,7 +88,13 @@ namespace Roman_numerals
                     }
                 }
 
-                Console.WriteLine("The corresponding numbers are:" + " " + total);
+                // Om ingen ogiltig inmatning hittades, visa resultatet
+                if (!invalidInput)
+                {
+                    Console.WriteLine("The corresponding number is: " + total);
+                }
+
+                // Fråga om användaren vill spela igen
                 Console.WriteLine("Would you like to play again? Y/N");
                 bajskorv = Console.ReadLine().ToUpper();
             }
